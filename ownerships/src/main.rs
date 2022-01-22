513321619b21153fdda54d89e52cbee19a55e16a
silloi <silloi1993@gmail.com>
2022-01-22 15:31:02 +0900

    complete ch04-02

--- ownerships/src/main.rs
+++ ownerships/src/main.rs
@@ -35,16 +35,59 @@ fn main() {
 
     let s1 = String::from("hello");
 
-    let (s2, len) = calculate_length(s1);
+    // let (s2, len) = calculate_length(s1);
 
-    println!("The length of '{}' is {}.", s2, len);
+    // println!("The length of '{}' is {}.", s2, len);
+
+    let len = calculate_length(&s1);
+
+    println!("The length of '{}' is {}.", s1, len);
+
+    let mut s = String::from("hello");
+
+    change(&mut s);
+
+    {
+        let r1 = &mut s;
+    }
+  
+    let r2 = &mut s;
+
+    // println!("{}, {}", r1, r2);
+
+    let mut s = String::from("hello");
+
+    let r1 = &s;
+    let r2 = &s;
+    println!("{} and {}", r1, r2);
+
+    let r3 = &mut s;
+    println!("{}", r3);
+
+    // println!("{}, {}, and {}", r1, r2, r3);
+
+    let reference_to_nothing = dangle();
 }
 
-fn calculate_length(s: String) -> (String, usize) {
-    let length = s.len();
+fn dangle() -> String {
+    let s = String::from("hello");
 
-    (s, length)
+    s
 }
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world!");
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+// fn calculate_length(s: String) -> (String, usize) {
+//     let length = s.len();
+// 
+//     (s, length)
+// }
 
 fn gives_ownership() -> String {
     let some_string = String::from("yours");
