093d399fcfc8d7251b62dead6a36c62b2b1a5827
silloi <silloi1993@gmail.com>
2022-01-24 08:29:25 +0900

    complete ch10

--- chapter10/src/lib.rs
+++ chapter10/src/lib.rs
@@ -1,3 +1,15 @@
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
 pub mod aggregator {
 
 use std::fmt::Display;
--- chapter10/src/main.rs
+++ chapter10/src/main.rs
@@ -1,5 +1,46 @@
 use chapter10::aggregator::{Summary, Tweet, NewsArticle};
 
+use std::fmt::Display;
+
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
 struct Point<X1, Y1> {
     x: X1,
     y: Y1,
@@ -38,6 +79,36 @@ fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
 }
 
 fn main() {
+    let s: &'static str = "I have a static lifetime.";
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+
+    let string1 = String::from("long string is long");
+
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
+
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+
+    {
+        let x = 5;
+        let r = &x;
+
+        println!("r: {}", r);
+
+    }
+
     let article = NewsArticle {
         headline: String::from("Penguins win the Stanley Cup Championship!"),
         location: String::from("Pittsburgh, PA, USA"),
