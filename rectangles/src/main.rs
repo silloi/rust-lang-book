8464c6376c364e3a79e145f4891b4eb61ecda73e
silloi <silloi1993@gmail.com>
2022-01-23 11:38:48 +0900

    complete 05-02

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -13,6 +14,16 @@ fn main() {
         "This area of the rectangle is {} square pixels.",
         area(&rect1)
     );
+
+    println!("rect1 is {:#?}", rect1);
+
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+
+    dbg!(&rect1);
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
