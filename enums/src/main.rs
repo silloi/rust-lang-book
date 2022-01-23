ad9f918fe4da882ef3a06dee242a46e40bd0e122
silloi <silloi1993@gmail.com>
2022-01-23 12:31:05 +0900

    complete ch06

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -73,6 +73,27 @@ enum IpAddr {
 // fn route(ip_kind: IpAddrKind) {}
 
 fn main() {
+    let config_max = Some(3u8);
+    // match config_max {
+    //     Some(max) => println!("The maximum is configured to be {}", max),
+    //     _ => (),
+    // }
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    }
+
+    let coin = Coin::Penny;
+    let mut count = 0;
+    // match coin {
+    //     Coin::Quarter(state) => println!("State quarter from {:?}!", state),
+    //     _ => count + 1,
+    // }
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
+
     let dice_roll = 9;
     match dice_roll {
         3 => add_fancy_hat(),
