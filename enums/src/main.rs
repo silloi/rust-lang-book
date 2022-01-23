a9d0b751ff414165ac424b2a5c5612a505e98177
silloi <silloi1993@gmail.com>
2022-01-23 12:19:01 +0900

    complete 06-02

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,44 @@
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+fn move_player(num_space: u8) {}
+fn reroll() {}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
 enum Message {
     Quit,
     Move { x: i32, y: i32 },
@@ -32,6 +73,18 @@ enum IpAddr {
 // fn route(ip_kind: IpAddrKind) {}
 
 fn main() {
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => (),
+        
+    }
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
     let some_number = Some(5);
     let some_string = Some("a string");
 
