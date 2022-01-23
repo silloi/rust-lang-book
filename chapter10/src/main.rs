ce49e8348501687e1d214db70c0b26ed4359d9e5
silloi <silloi1993@gmail.com>
2022-01-24 00:01:25 +0900

    complete ch10-02

--- chapter10/src/main.rs
+++ chapter10/src/main.rs
@@ -1,4 +1,31 @@
-fn largest(list: &[i32]) -> i32 {
+use chapter10::aggregator::{Summary, Tweet, NewsArticle};
+
+struct Point<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
+
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+
+// impl<T> Point<T> {
+//     fn x(&self) -> &T {
+//         &self.x
+//     }
+// }
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
     let mut largest = list[0];
 
     for &item in list {
@@ -11,10 +38,54 @@ fn largest(list: &[i32]) -> i32 {
 }
 
 fn main() {
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best \
+             hockey team in the NHL.",
+        ),
+    };
+
+    println!("New article available! {}", article.summarize());
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+ 
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+
+    let p = Point { x: 5, y: 10 };
+
+    // println!("p.x = {}", p.x());
+
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
+
+    // let wont_work = Point { x: 5, y: 4.0 };
+
     let number_list = vec![34, 50, 25, 100, 65];
 
     let result = largest(&number_list);
     println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
 
