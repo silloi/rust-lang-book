96f3ba6308c4ee03ccf03dbbdd1e72e411de4151
silloi <silloi1993@gmail.com>
2022-01-23 11:58:22 +0900

    update enum

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,29 +1,33 @@
-enum IpAddrKind {
-    V4,
-    V6,
+enum IpAddr {
+    V4(String),
+    V6(String),
 }
 
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
-}
+// struct IpAddr {
+//     kind: IpAddrKind,
+//     address: String,
+// }
 
-fn route(ip_kind: IpAddrKind) {}
+// fn route(ip_kind: IpAddrKind) {}
 
 fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
+    // let four = IpAddrKind::V4;
+    // let six = IpAddrKind::V6;
 
-    route(IpAddrKind::V4);
-    route(IpAddrKind::V6);
+    // route(IpAddrKind::V4);
+    // route(IpAddrKind::V6);
 
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
+    // let home = IpAddr {
+    //     kind: IpAddrKind::V4,
+    //     address: String::from("127.0.0.1"),
+    // };
 
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
+    // let loopback = IpAddr {
+    //     kind: IpAddrKind::V6,
+    //     address: String::from("::1"),
+    // };
+
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+
+    let loopback = IpAddr::V6(String::from("::1"));
 }
