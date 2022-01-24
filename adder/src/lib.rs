f4b23e31aadb20dc5a9a849a05debe55680062e3
silloi <silloi1993@gmail.com>
2022-01-25 08:43:15 +0900

    complete 11-01

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -10,4 +10,13 @@ mod tests {
     fn it_adds_two() {
         assert_eq!(4, add_two(2));
     }
+
+    #[test]
+    fn it_works () -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
 }
