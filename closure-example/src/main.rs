8285130046d954f2753c17486711e43dec07c953
西山 雄大 <nishiyamayudai@team-lab.com>
2022-01-30 11:37:29 +0900

    complete 13-01

--- closure-example/src/main.rs
+++ closure-example/src/main.rs
@@ -1,6 +1,47 @@
 use std::thread;
 use std::time::Duration;
 
+    #[test]
+    fn call_with_different_values() {
+        let mut c = Cacher::new(|a| a);
+
+        let v1 = c.value(1);
+        let v2 = c.value(2);
+
+        // assert_eq!(v2, 2);
+    }
+
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value { 
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
 fn simulated_expensive_calculation(intensity: u32) -> u32 {
     println!("calculating slowly...");
     thread::sleep(Duration::from_secs(2));
@@ -8,6 +49,32 @@ fn simulated_expensive_calculation(intensity: u32) -> u32 {
 }
 
 fn main() {
+    let x = vec![1, 2, 3];
+
+    let equal_to_x = move |z| z == x;
+
+    println!("can't use x here: {:?}", x);
+
+    let y = vec![1, 2, 3];
+
+    assert!(equal_to_x(y));
+
+    let x = 4;
+
+    let equal_to_x = |z| z == x;
+
+    // fn equal_to_x(z: i32) -> bool {
+    //     z == x
+    // }
+    let y = 4;
+
+    assert!(equal_to_x(y));
+
+    let example_closure = |x| x;
+
+    let s = example_closure(String::from("hello"));
+    // let n = example_closure(5);
+
     let simulated_user_specified_value = 10;
     let simulated_random_number = 7;
 
@@ -15,22 +82,22 @@ fn main() {
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
+    let mut expensive_result = Cacher::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
     if intensity < 25 {
-        println!(
-            "Today, do {} pushups!",
-            simulated_expensive_calculation(intensity)
-        );
-        println!(
-            "Next, do {} situps!",
-            simulated_expensive_calculation(intensity)
-        );
+        println!("Today, do {} pushups!", expensive_result.value(intensity));
+        println!("Next, do {} situps!", expensive_result.value(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
             println!(
                 "Today, run for {} minutes!",
-                simulated_expensive_calculation(intensity)
+                 expensive_result.value(intensity)
             );
         }
     }
